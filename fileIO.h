9e1737e304a75dd0d8b42763df156f1cb190ae55
lauralelea <165846224+lauralelea@users.noreply.github.com>
2024-04-03 01:53:43 +0200

    Add files via upload

---
+++ fileIO.h
@@ -0,0 +1,11 @@
+#ifndef FILEIO_H
+#define FILEIO_H
+
+#include <stdio.h>
+
+void readFromFile(const char* filePath, int* output);
+void readFromCSV(const char* filePath);
+void writeToFile(const char* filePath, const char* message);
+FILE* openFile(const char* filePath, const char* mode);
+
+#endif 
